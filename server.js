bb0d7dbf92aaba1228f3ad2563ab749b7d028b68
Fatima El Hlali <elhilali-1991@hotmail.com>
2025-02-25 17:21:48 +0100

    #4 GET-request voor student verwijderd omdat deze niet nodig was

--- server.js
+++ server.js
@@ -15,16 +15,13 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', async function (request, response) {
-  const personResponse = await fetch( 'https://fdnd.directus.app/items/person/?sort=name&fields=id,name,squads.squad_id.name,most_energy&filter={"squads":{"squad_id":{"name":"1G"}}}')
+  const personResponse = await fetch('https://fdnd.directus.app/items/person/?sort=name&fields=id,name,squads.squad_id.name&filter={"squads":{"squad_id":{"name":"1G"}}}')
+
   const personResponseJSON = await personResponse.json()
   response.render('index.liquid', {persons: personResponseJSON.data})
 })
 
-app.get('/student/:id', async function (request, response) {
-  const personDetailResponse = await fetch('https://fdnd.directus.app/items/person/' + request.params.id)
-  const personDetailResponseJSON = await personDetailResponse.json()
-    response.render('student.liquid', {person: personDetailResponseJSON.data, squads: squadResponseJSON.data})
-})
+
 
 app.post('/', async function (request, response) {
   await fetch('https://fdnd.directus.app/items/messages/', {
