01533bcb922462ee46cb76f5b30878ed0187c3ca
Clarice-COD <clariceboerlijst@gmail.com>
2025-02-27 23:46:11 +0100

    /chat naar /index aangepast
    Comments zijn nog niet werkend op deze pagina

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ app.get('/drama', async function (request, response) {
 
 
 
-app.get('/chat', async function (request, response) {
+app.get('/index', async function (request, response) {
   const personResponse = await fetch('https://fdnd.directus.app/items/person/?sort=name&fields=id,name,squads.squad_id.name&filter={"squads":{"squad_id":{"name":"1G"}}}')
   const personResponseJSON = await personResponse.json()
 
@@ -93,42 +93,42 @@ app.get('/chat', async function (request, response) {
     msg.likes = likes;
   })
   
-  response.render('chat.liquid', {persons: personResponseJSON.data, messages: messages})
+  response.render('index.liquid', {persons: personResponseJSON.data, messages: messages})
 })
 
 
 
-// CODE bewerken
+// CHATBOX
 
 const squadResponse = await fetch('https://fdnd.directus.app/items/squad?filter={"_and":[{"cohort":"2425"},{"tribe":{"name":"FDND Jaar 1"}}]}')
 const squadResponseJSON = await squadResponse.json()
 
-app.get('/chat', async function (request, response) {
+app.get('/index', async function (request, response) {
   // Haal berichten op voor het team
   const messagesResponse = await fetch(`https://fdnd.directus.app/items/messages?limit=-1&filter={"for" : "Team Zen / Vragen"}`);
   const messagesResponseJSON = await messagesResponse.json();
  
-  response.render('chat.liquid', {
+  response.render('index.liquid', {
     // teamName: teamName,
     //squads: squadResponseJSON.data,
     messages: messagesResponseJSON.data
   });
 })
  
-app.post('/chat', async function (request, response) {
+app.post('/index', async function (request, response) {
   await fetch('https://fdnd.directus.app/items/messages/', {
     method: 'POST',
     body: JSON.stringify({
       // teamName: teamName,
       for: "Team Zen / Vragen",
-      text: request.body.message
+      text: request.body.message // post
     }),
     headers: {
       'Content-Type': 'application/json;charset=UTF-8'
     }
   });
   
-  response.redirect('/chat')
+  response.redirect('/index')
 })
 
 // stap 1: nieuwe /like POST request. Met een unique message id (request.body.id)
@@ -144,7 +144,7 @@ app.post('/like', async function (request, response) {
     }
   });
   
-  response.redirect('/chat')
+  response.redirect('/index')
 })
  
 
